18b61bd5704c3ca2288934b213634809320411e4
MrTspin <bryan.christopher.hart@gmail.com>
2016-10-04 21:37:21 -0400

    Bryan Hart
    Formatting

---
+++ formatting.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+main()
+{
+    printf("source code:\n------------\n");
+    char origText, nonComm;
+    while(!EOF)
+    {
+        // TODO printer with comments
+    }
+    printf("source code without comments:\n-----------------------------\n");
+
+    {
+        // TODO place holder for print code without comments
+    }
+
+    printf("tokens:\n-------\n");
+    /*char token[]="var";
+    int tokenVal = 29;
+    int numTokens=1;*/
+    for (int i=0;i<numTokens;i++) // TODO edit parameter length
+        printf("%-13s%d",token,tokenVal);
+
+}
